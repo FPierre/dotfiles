bee9524f54e1a91863a04b29e6acc20914cd0ac1
FPierre <pflauder@gmail.com>
2021-03-31 21:44:40 +0200

    refacto: comment useless Fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,22 +22,22 @@ starship init fish | source
 
 # Autojump
 # Python error fix: https://stackoverflow.com/a/61608129
-if test -f /home/pierre/.autojump/share/autojump/autojump.fish
-  . /home/pierre/.autojump/share/autojump/autojump.fish
-end
+# if test -f /home/pierre/.autojump/share/autojump/autojump.fish
+#   . /home/pierre/.autojump/share/autojump/autojump.fish
+# end
 
 # MySQL
-set -gx SQL_PORT 3306
+# set -gx SQL_PORT 3306
 
 # Yarn
-set -gx PATH $PATH:/usr/bin/yarn
+# set -gx PATH $PATH:/usr/bin/yarn
 
 # Java
-set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
-set -gx PATH $PATH:$JAVA_HOME/bin
+# set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
+# set -gx PATH $PATH:$JAVA_HOME/bin
 
 # Android studio
-set -gx ANDROID_HOME /home/pierre/Workspace/mobile/Android/Sdk
+# set -gx ANDROID_HOME /home/pierre/Workspace/mobile/Android/Sdk
 
 # Helm
-set -gx PATH $PATH:/opt/helm
+# set -gx PATH $PATH:/opt/helm
